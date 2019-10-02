3d61d3055bc512066ddf0a92991069c32795407e
yiranzi <570932296@qq.com>
2019-10-02 16:53:02 +0800

    update: test

--- src/pages/timeFlies/redux/server.js
+++ src/pages/timeFlies/redux/server.js
@@ -5,10 +5,11 @@ const pageHost = host + "/eventStream";
 
 const server = {
   getEventStreamList: async () => {
-    console.log('start server testRouter')
+    console.log("start server testRouter");
     // const url = "/getEventStreamList";
     const url = "/testRouter";
-    const result = await ajax.get(pageHost + url);
+    const result = await ajax.get(url);
+    // const result = await ajax.get(pageHost + url);
     return result;
   }
 };
